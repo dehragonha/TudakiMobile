2005d593b6054aa129903ef17834145437dba11d
jillesragonha <a1s2d3>
2015-02-05 16:06:33 -0200

    Encaminhamento do projeto inicial, com tela de splash e login

---
+++ app/src/main/java/br/com/agilles/tudakimobile/view/LoginActivity.java
@@ -0,0 +1,18 @@
+package br.com.agilles.tudakimobile.view;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+import br.com.agilles.tudakimobile.R;
+
+/**
+ * Created by Inteligencia on 03/02/2015.
+ */
+public class LoginActivity extends Activity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.login_layout);
+
+    }
+}
